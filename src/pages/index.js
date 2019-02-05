4c87139ad2a8083f99efbfee2da9076dfe2c299f
Amelia Schulz <amelia.schulz@gmail.com>
2019-02-05 12:50:18 -0500

    adds puppy homepage

---
+++ src/pages/index.js
@@ -0,0 +1,16 @@
+import React from "react"
+import { Link } from "gatsby"
+
+import Layout from "../components/layout"
+import Image from "../components/image"
+import SEO from "../components/seo"
+
+const IndexPage = () => (
+  <Layout>
+    <h1>Hey Gatsby!</h1>
+    <p>hello world</p>
+    <img  src="https://images.pexels.com/photos/39317/chihuahua-dog-puppy-cute-39317.jpeg?cs=srgb&dl=animal-chihuahua-cute-39317.jpg&fm=jpg" alt="puppy in a teacup"/>
+  </Layout>
+)
+
+export default IndexPage
