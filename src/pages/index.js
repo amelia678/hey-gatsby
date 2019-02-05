e58968a18c289093ffcdf0bda0c25eb44605b0b9
Amelia Schulz <amelia.schulz@gmail.com>
2019-02-05 12:57:49 -0500

    adds about page

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,7 +8,8 @@ import SEO from "../components/seo"
 const IndexPage = () => (
   <Layout>
     <h1>Hey Gatsby!</h1>
-    <p>hello world</p>
+    <h2>hello world</h2>
+    <p>hello pupper</p>
     <img  src="https://images.pexels.com/photos/39317/chihuahua-dog-puppy-cute-39317.jpeg?cs=srgb&dl=animal-chihuahua-cute-39317.jpg&fm=jpg" alt="puppy in a teacup"/>
   </Layout>
 )
